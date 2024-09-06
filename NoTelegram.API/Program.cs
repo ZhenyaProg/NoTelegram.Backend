56283ed0f6a4d6cb3b9fb81e93c5fce7963d7daa
Zhenya <schoolbast@mail.ru>
2024-09-06 15:24:31 +0300

    initial commit

---
+++ NoTelegram.API/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
